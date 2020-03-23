90abc522d7352e18167cd97ddd13149f01ab4169
JACOB BOULLION <jpboonie@gmail.com>
2020-03-23 17:07:38 -0500

    New Changes

--- app/src/main/java/com/example/peopletailwireframes/PeopleTailMainView.java
+++ app/src/main/java/com/example/peopletailwireframes/PeopleTailMainView.java
@@ -2,7 +2,9 @@ package com.example.peopletailwireframes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class PeopleTailMainView extends AppCompatActivity {
 
@@ -10,5 +12,35 @@ public class PeopleTailMainView extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_people_tail_main_view);
+    }
+
+    public void onClick(View view){
+
+        switch (view.getId()){
+            case R.id.home_button:
+
+                Intent homeintent = new Intent(/*[thisactivity.java],*[homeactivity.class]*/);
+
+                startActivity(homeintent);
+
+                break;
+
+            case R.id.search_button:
+                Intent searchintent = new Intent(/*[thisactivity.java],[searchactivity.class]*/);
+
+                startActivity(searchintent);
+                break;
+
+            case R.id.profile_button:
+
+                Intent profileintent = new Intent(/*[thisactivity.java],[profileactivity.class]*/);
+
+                startActivity(profileintent);
+
+                break;
+
+        }
+
+
     }
 }
