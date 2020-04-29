0a205203838472dafb1783fe3fbcb1114cfc71e3
pratikpdl1996 <62625376+pratikpdl1996@users.noreply.github.com>
2020-04-29 13:24:48 -0500

    Add Review and Add Retailers
    6 drawable folders in res.
    added my java activity and xml files

---
+++ app/src/main/java/com/example/peopletailwireframes/AddReview.java
@@ -0,0 +1,43 @@
+package com.example.peopletailwireframes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class AddReview extends AppCompatActivity {
+
+
+    private Button button1, button2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_review);
+
+        button1 = (Button) findViewById(R.id.add_retailers);
+        button2 = (Button) findViewById(R.id.btn_publish);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+                                       @Override
+                                       public void onClick(View v) {
+                                           SecondActivity();
+                                       }
+                                   }
+        );
+
+
+    }
+
+    public void SecondActivity(){
+
+        Intent intent = new Intent(this, AddRetailers.class);
+
+        startActivity(intent);
+    }
+
+
+}
+
